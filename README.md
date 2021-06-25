10e4ae6a11767a426b07382a5a7bd0e99647e3a6
Satyendra Kumar <satyen@us.ibm.com>
2021-06-25 16:23:27 -0500

    Fix some issues and added rich set of options

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # What is this?
 
-Tokenizes a String with double quotes and escaped double quotes
+Tokenizes input string with double quote and single quote with rich set of options. Handles escaped quote within each. Supports single-char tokens and additional word boundaries.
 
 # Installation
 
@@ -8,14 +8,14 @@ Tokenizes a String with double quotes and escaped double quotes
 
 Then...
 
+* Example
 ```
 const dqtokenizer = require('dqtokenizer');
 
 const str = 'eval hie "SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) == \\"90\\")" print nohia attributes "partNum"';
 const tokens = dqtokenizer.tokenize(str, {
     includeDoubleQuote: false,
-    removeBackslashOfInternalDoubleQuote: true,
-    fillMissingLastDoubleQuote: true
+    removeBackslashOfInternalDoubleQuote: true
 });
 console.log(`str: ${str}`);
 console.log(`tokens: ${tokens}`);
@@ -25,10 +25,78 @@ str: eval hie "SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) =
 tokens: eval,hie,SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) == "90"),print,nohia,attributes,partNum
 ```
 
+* More Examples
+```
+    const dqtokenizer = require('dqtokenizer');
+
+    const testTokenize = (str, options) => {
+        const tokens = dqtokenizer.tokenize(str, options);
+        console.log();
+        console.log(`str: ${str}`);
+        console.log(`tokens:`);
+        tokens.forEach((token, index) => console.log(`\t${index}: ${token}`));
+    }
+
+    // Example 1
+    testTokenize('eval hie "SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) == \\"90\\")" print nohia attributes "partNum"');
+
+    // Example 2
+    testTokenize('eval hie "SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) == \\"90\\")" print nohia attributes "partNum"', {
+        includeDoubleQuote: false,
+        removeBackslashOfInternalDoubleQuote: true,
+    });
+
+    // Example 3
+    testTokenize(`{"letters" : '321"}{}"'}{'{}{{}"': "stack{}}{"}`, {
+        additionalBoundaryChars: [],
+        singleCharTokens: ['(', ')', '{', '}', '[', ']', ':'],
+    });
+
+// Output:
+str: eval hie "SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) == \"90\")" print nohia attributes "partNum"
+tokens:
+        0: eval
+        1: hie
+        2: "SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) == \"90\")"
+        3: print
+        4: nohia
+        5: attributes
+        6: "partNum"
+
+str: eval hie "SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) == \"90\")" print nohia attributes "partNum"
+tokens:
+        0: eval
+        1: hie
+        2: SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) == "90")
+        3: print
+        4: nohia
+        5: attributes
+        6: partNum
+
+str: {"letters" : '321"}{}"'}{'{}{{}"': "stack{}}{"}
+tokens:
+        0: {
+        1: "letters"
+        2: :
+        3: '321"}{}"'
+        4: }
+        5: {
+        6: '{}{{}"'
+        7: :
+        8: "stack{}}{"
+        9: }
+```
+
 ## Options
 
-dqtokenizer supports 3 options, all of which are optional:
+dqtokenizer supports a second paramater, which is optional. It contains following options:
 
+* *additionalBoundaryChars* - _array of chars (Defaults to [',', ';'])
+* *singleCharTokens* - _array of chars (Defaults to ['(', ')', '{', '}', '[', ']', ':'])
 * *includeDoubleQuote* - _boolean_ (Defaults to true)
+* *includeSingleQuote* - _boolean_ (Defaults to true)
 * *removeBackslashOfInternalDoubleQuote* - _boolean_ (Defaults to false)
+* *removeBackslashOfInternalSingleQuote* - _boolean_ (Defaults to false)
 * *fillMissingLastDoubleQuote* - _boolean_ (Defaults to true)
+* *fillMissingLastSingleQuote* - _boolean_ (Defaults to true)
+* *debug* - _boolean_ (Defaults to false)
--- index.js
+++ index.js
@@ -1,29 +1,91 @@
-const tokenize = (str,
-    { includeDoubleQuote, removeBackslashOfInternalDoubleQuote, fillMissingLastDoubleQuote }
-        =
-        {
-            includeDoubleQuote: true, removeBackslashOfInternalDoubleQuote: false, fillMissingLastDoubleQuote: true
-        }) => {
+const tokenize = (str, {
+    additionalBoundaryChars = [',', ';'],
+    singleCharTokens = ['(', ')', '{', '}', '[', ']', ':'],
+    includeDoubleQuote = true,
+    includeSingleQuote = true,
+    removeBackslashOfInternalDoubleQuote = false,
+    removeBackslashOfInternalSingleQuote = false,
+    fillMissingLastDoubleQuote = true,
+    fillMissingLastSingleQuote = true,
+    debug = false
+} = {}) => {
+    // {
+    //     additionalBoundaryChars,
+    //     singleCharTokens,
+    //     includeDoubleQuote,
+    //     includeSingleQuote,
+    //     removeBackslashOfInternalDoubleQuote,
+    //     removeBackslashOfInternalSingleQuote,
+    //     fillMissingLastDoubleQuote,
+    //     fillMissingLastSingleQuote
+    // }
+    //     =
+    //     {
+    //         additionalBoundaryChars: [','],
+    //         singleCharTokens: ['(', ')', '{', '}', '[', ']', ':'],
+    //         includeDoubleQuote: true,
+    //         includeSingleQuote: true,
+    //         removeBackslashOfInternalDoubleQuote: false,
+    //         removeBackslashOfInternalSingleQuote: false,
+    //         fillMissingLastDoubleQuote: true,
+    //         fillMissingLastSingleQuote: true
+    //     }) ={
+    if (debug) {
+        const options = {
+            additionalBoundaryChars,
+            singleCharTokens,
+            includeDoubleQuote,
+            includeSingleQuote,
+            removeBackslashOfInternalDoubleQuote,
+            removeBackslashOfInternalSingleQuote,
+            fillMissingLastDoubleQuote,
+            fillMissingLastSingleQuote,
+            debug
+        }
+
+        console.log('options:', JSON.stringify(options, null, 2));
+    }
     let dqstart = false;
+    let sqstart = false;
     let lastchar = null;
-    let wordstart = true;
+    let wordstart = false;
     let startIndex = -1;
     const tokens = [];
     const addToken = (token) => {
         if (removeBackslashOfInternalDoubleQuote) {
             tokens.push(token.replace(/\\"/g, '"'));
-        } else {
-            tokens.push(token);
+            return;
         }
+
+        if (removeBackslashOfInternalSingleQuote) {
+            tokens.push(token.replace(/\\'/g, `'`));
+            return;
+        }
+
+        tokens.push(token);
+    }
+
+    const isSingleCharToken = (c) => {
+        if (singleCharTokens === undefined || singleCharTokens === null) {
+            return false;
+        }
+
+        return singleCharTokens.some(achar => achar === c);
+    }
+
+    const isBoundaryChar = (c) => {
+        if (additionalBoundaryChars === undefined || additionalBoundaryChars === null) {
+            return false;
+        }
+
+        return additionalBoundaryChars.some(achar => achar === c);
     }
 
     for (var i = 0; i < str.length; i++) {
         const c = str.charAt(i);
         // Whitespace
-        if (/\s/.test(c)) {
-            if (dqstart) {
-
-            } else {
+        if (/\s/.test(c) || isBoundaryChar(c)) {
+            if (!dqstart && !sqstart) {
                 if (wordstart) {
                     wordstart = false;
                     addToken(str.substring(startIndex, i));
@@ -32,6 +94,7 @@ const tokenize = (str,
             // Double quote
         } else if (c === '"') {
             if (lastchar != null && lastchar === '\\') {
+            } else if (sqstart) {
             } else if (dqstart) {
                 dqstart = false;
                 if (includeDoubleQuote) {
@@ -41,11 +104,45 @@ const tokenize = (str,
                 }
                 wordstart = false;
             } else {
+                if (wordstart) {
+                    addToken(str.substring(startIndex, i));
+                }
                 dqstart = true;
                 wordstart = true;
                 startIndex = i;
             }
+            // Single quote
+        } else if (c === `'`) {
+            if (lastchar != null && lastchar === '\\') {
+            } else if (dqstart) {
+            } else if (sqstart) {
+                sqstart = false;
+                if (includeSingleQuote) {
+                    addToken(str.substring(startIndex, i + 1));
+                } else {
+                    addToken(str.substring(startIndex + 1, i));
+                }
+                wordstart = false;
+            } else {
+                if (wordstart) {
+                    addToken(str.substring(startIndex, i));
+                }
+                sqstart = true;
+                wordstart = true;
+                startIndex = i;
+            }
             // Non-whitespace or doublequote
+        } else if (isSingleCharToken(c)) {
+            if (sqstart || dqstart) {
+
+            } else {
+                if (wordstart) {
+                    addToken(str.substring(startIndex, i));
+                }
+
+                wordstart = false;
+                addToken(str.substring(i, i + 1));
+            }
         } else {
             if (wordstart == false) {
                 wordstart = true;
@@ -66,6 +163,12 @@ const tokenize = (str,
                 } else {
                     addToken(str.substring(startIndex + 1, i + 1));
                 }
+            } else if (sqstart) {
+                if (includeSingleQuote) {
+                    addToken(str.substring(startIndex, i + 1) + fillMissingLastSingleQuote ? `'` : '');
+                } else {
+                    addToken(str.substring(startIndex + 1, i + 1));
+                }
             }
         }
     }
@@ -73,13 +176,27 @@ const tokenize = (str,
     return tokens;
 }
 
-// const str = 'eval hie "SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) == \\"90\\")" print nohia attributes "partNum"';
-// const tokens = tokenize(str, {
-//     includeDoubleQuote: false,
-//     removeBackslashOfInternalDoubleQuote: true,
-//     fillMissingLastDoubleQuote: true
-// });
-// console.log(`str: ${str}`);
-// console.log(`tokens: ${tokens}`);
+const test = true;
+if (test) {
+    const testTokenize = (str, options) => {
+        const tokens = tokenize(str, options);
+        console.log();
+        console.log(`str: ${str}`);
+        console.log(`tokens:`);
+        tokens.forEach((token, index) => console.log(`\t${index}: ${token}`));
+    }
+
+    testTokenize('eval,; hie "SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) == \\"90\\")" print nohia attributes "partNum"');
+
+    testTokenize('eval hie "SUBSET(?x, allComponents(SQ_Server_MTM), Leftstr(?x.partNum, 2) == \\"90\\")" print nohia attributes "partNum"', {
+        includeDoubleQuote: false,
+        removeBackslashOfInternalDoubleQuote: true,
+    });
+
+    testTokenize(`{"letters" : '321"}{}"'}{'{}{{}"': "stack{}}{"}`, {
+        additionalBoundaryChars: [],
+        singleCharTokens: ['(', ')', '{', '}', '[', ']', ':'],
+    });
+}
 
 exports.tokenize = tokenize;
